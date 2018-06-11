37452f5c1f48d022e1a6623f8ab72f061b95275c
Daniel <dfdazac@gmail.com>
2018-06-11 17:10:29 +0200

    More straightforward explanations, added plotting

--- vge_gradcam.py
+++ vge_gradcam.py
@@ -5,6 +5,9 @@ from train.trainer_loader import TrainerLoader
 from utils.data.data_prep import DataPreparation
 import utils.arg_parser
 from utils.misc import get_split_str
+from PIL import Image
+import matplotlib.pyplot as plt
+import os
 
 # Get default arguments
 args = utils.arg_parser.get_args()
@@ -48,11 +51,20 @@ trainer = trainer_creator(args, model, dataset, data_loader, logger=None, device
 
 # Given an image id, retrieve image and label
 # (assuming the image exists in the corresponding dataset!)
+images_path = 'data/cub/images/'
 img_id = '121.Grasshopper_Sparrow/Grasshopper_Sparrow_0078_116052.jpg'
+raw_image = Image.open(os.path.join(images_path, img_id))
 image_input = dataset.get_image(img_id).unsqueeze(dim=0)
 label = dataset.get_class_label(img_id)
 
-# Generate explanation
-explanation = trainer.eval_step(image_input, (img_id,) , label)[0]
-print('Image id: {:s}\nExplanation: {:s}'.format(explanation['image_id'], explanation['caption']))
+# Generate explanation (skip EOS)
+outputs = model.generate_sentence(image_input, trainer.start_word, trainer.end_word, label)[:-1]
+explanation = ' '.join([dataset.vocab.get_word_from_idx(idx.item()) for idx in outputs])
+
+# Plot results
+plt.imshow(raw_image)
+plt.title(explanation)
+plt.axis('off')
+plt.show()
+
 

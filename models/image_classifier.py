0cab62b75114f3d3fb293a6f21a29e3eabdb94f2
Daniel <dfdazac@gmail.com>
2018-06-16 15:36:23 +0200

    Added cbp to forward pass

--- models/image_classifier.py
+++ models/image_classifier.py
@@ -1,14 +1,17 @@
 import torch.nn as nn
 import torch.nn.functional as F
 from .pretrained_models import PretrainedModel
+from .bilinear import CompactBilinearPooling
+
 
 class BilinearImageClassifier(nn.Module):
-    def __init__(self, input, bilinear_dim, num_classes):
+    def __init__(self, model_name, bilinear_dim, num_classes):
         super(BilinearImageClassifier, self).__init__()
 
-        self.vision_model = PretrainedModel(input, layers_to_truncate=1)
+        self.vision_model = PretrainedModel(model_name, layers_to_truncate=1)
         img_feat_size = self.vision_model.output_size
 
+        self.cbp = CompactBilinearPooling(img_feat_size, img_feat_size, bilinear_dim)
         self.linear = nn.Linear(bilinear_dim, num_classes)
         self.init_weights()
         self.output_size = num_classes
@@ -22,6 +25,6 @@ class BilinearImageClassifier(nn.Module):
 
     def forward(self, image):
         image_features = self.vision_model(image)
-        outputs = F.relu(self.linear1(image_features))
-        outputs = F.relu(self.linear2(outputs))
-        return outputs
+        bilinear_features = self.cbp(image_features, image_features)
+        logits = self.linear(bilinear_features)
+        return logits

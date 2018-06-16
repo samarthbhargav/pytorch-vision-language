80ae402f4561ca3cbbbddcb441397d7a385df65c
Daniel <dfdazac@gmail.com>
2018-06-16 19:16:50 +0200

    Added fixes so it works with VGG16

--- models/image_classifier.py
+++ models/image_classifier.py
@@ -7,24 +7,28 @@ class BilinearImageClassifier(nn.Module):
     def __init__(self, model_name, bilinear_dim, num_classes):
         super(BilinearImageClassifier, self).__init__()
 
-        self.vision_model = PretrainedModel(model_name, layers_to_truncate=1)
-        img_feat_size = self.vision_model.output_size
+        self.vision_model = PretrainedModel(model_name, layers_to_truncate=3)
 
-        self.cbp = CompactBilinearPooling(img_feat_size, img_feat_size, bilinear_dim)
+        # First hack for VGG16
+        img_feat_size = 512
+
+        self.cbp = CompactBilinearPooling(img_feat_size, bilinear_dim)
         self.linear = nn.Linear(bilinear_dim, num_classes)
         self.init_weights()
         self.output_size = num_classes
 
     def init_weights(self):
-        self.linear1.weight.data.uniform_(-0.1, 0.1)
-        self.linear1.bias.data.fill_(0)
+        self.linear.weight.data.uniform_(-0.1, 0.1)
+        self.linear.bias.data.fill_(0)
 
     def state_dict(self, *args, full_dict=False, **kwargs):
         return super().state_dict(*args, **kwargs)
 
     def get_bilinear_features(self, image):
-        image_features = self.vision_model(image)
-        bilinear_features = self.cbp(image_features, image_features)
+        # Second hack for VGG16: reshaping feature maps
+        image_features = self.vision_model(image).view(-1, 512, 7, 7)
+        bilinear_features = self.cbp(image_features)
+        # TODO: element-wise signed square root layer
         return bilinear_features
 
     def forward(self, image):
